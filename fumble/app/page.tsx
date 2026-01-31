de237f39c762c7e15aa398cbdb881af6e07531b7
BalakeKarbon <blakekarbon@gmail.com>
2026-01-31 23:13:44 +0000

    Socket bones

--- fumble/app/page.tsx
+++ fumble/app/page.tsx
@@ -8,8 +8,41 @@ import PhotoCard from "./components/PhotoCard";
 import SwipeableCard from "./components/SwipeableCard";
 import LoginView from "./components/LoginView";
 
+import { socket } from "./socket.js"
+
 export default function Home() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
+
+  useEffect(() => {
+    if (socket.connected) {
+      onConnect();
+    }
+
+    function onConnect() {
+      setIsConnected(true);
+      setTransport(socket.io.engine.transport.name);
+
+      socket.io.engine.on("upgrade", (transport) => {
+        setTransport(transport.name);
+      });
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+      setTransport("N/A");
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+    };
+  }, []);
 
   if (!isAuthenticated) {
     return <LoginView onLoginSuccess={() => setIsAuthenticated(true)} />;
